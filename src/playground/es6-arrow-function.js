cb0cb53e185f28df97c7f31b44df118f076dba05
Thomas Gilmore <thomasgilmore95@gmail.com>
2020-05-15 19:05:26 -0400

    indecision app

--- src/playground/es6-arrow-function.js
+++ src/playground/es6-arrow-function.js
@@ -1,27 +1,19 @@
-// function square(x) {
-//   return x * x;
+const square = function (x) {
+    return x * x;
+}
+console.log(square(3));
+
+// const squareArrow = (x) => {
+//     return x * x;
 // };
 
-// console.log(square(3));
+const squareArrow = (x) => x * x;
 
-// // const squareArrow = (x) => {
-// //   return x * x;
-// // };
-
-// const squareArrow = (x) => x * x;
-
-// console.log(squareArrow(4));
-
-
-// Challenge - Use arrow functions
-// getFirstName('Mike Smith') -> "Mike"
-// Create regular arrow function
-// Create arrow function using shorthand syntax
+console.log(squareArrow(4));
 
 // const getFirstName = (fullName) => {
-//   return fullName.split(' ')[0];
+//     return fullName.split(' ')[0];
 // };
 
 const getFirstName = (fullName) => fullName.split(' ')[0];
-
-console.log(getFirstName('Andrew Mead'));
+console.log(getFirstName('Miske Smith'));
